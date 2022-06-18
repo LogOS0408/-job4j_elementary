ebf3121db78d0ad0cdbf691998dcf679ea563778
LogOS0408 <bsv-81@mail.ru>
2022-06-18 10:02:35 +0400

    3.1. Переназначение переменной [#232354]

---
+++ src/main/java/ru/job4j/VariableReassignment.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class VariableReassignment {
+    public static void main(String[] agrs) {
+        int size = 5;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
+    }
+}
