de9f57c8a57f6ec243b2893c7f4ac8e87593c31f
LogOS0408 <bsv-81@mail.ru>
2022-06-18 05:29:18 +0400

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/VariablesTwo.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+public class VariablesTwo {
+    public static void main(String[] agrs) {
+        byte age = 18;
+        System.out.println(age);
+        char gerder = 'M';
+        System.out.println(gerder);
+        float height = 1.86F;
+        System.out.println(height);
+        short brick = 10400;
+        System.out.println(brick);
+        int bacteria = 100000000;
+        System.out.println(bacteria);
+        long neptune = 4500000000L;
+        System.out.println(neptune);
+        boolean greencard = true;
+        System.out.println(greencard);
+        long size = 8589934592L;
+        System.out.println(size);
+
+    }
+}
