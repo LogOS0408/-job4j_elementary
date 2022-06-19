54100b0a39903f42be21372c9e210e27c3c2c481
LogOS0408 <bsv-81@mail.ru>
2022-06-19 04:25:34 +0400

    4. Методы [#231984]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
         int two = 2;
         int sixDiwTwo = six / two;
         int fiveMinusTwo = five - two;
-        int fourTimeTwo = four / two;
+        int fourTimeTwo = four * two;
         System.out.println(sixDiwTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
