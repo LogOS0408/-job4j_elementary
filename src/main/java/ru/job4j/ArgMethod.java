e031d707dfe515817493014bebc27401ec1005ba
LogOS0408 <bsv-81@mail.ru>
2022-06-19 05:50:34 +0400

    4.1. Аргументы [#295521]

---
+++ src/main/java/ru/job4j/ArgMethod.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class ArgMethod {
+    public static void hello(String name, int age) {
+        System.out.println("Hello," + name + "age = " + age);
+
+    }
+
+    public static void main(String[] agrs) {
+        String name = "Jab4j";
+        int age = 6;
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+    }
+
+}
