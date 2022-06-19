0ba4797c0c2e094315cd4b9b965f47cd57e393e4
LogOS0408 <bsv-81@mail.ru>
2022-06-19 05:10:17 +0400

    2. Переменные. [#172635 #313618]

--- src/main/java/ru/job4j/VariablesTwo.java
+++ src/main/java/ru/job4j/VariablesTwo.java
@@ -10,9 +10,9 @@ public class VariablesTwo {
         System.out.println(height);
         short brick = 10400;
         System.out.println(brick);
-        double bacteria = 1.325E27;
+        double bacteria = 1E+8;
         System.out.println(bacteria);
-        double neptune = 1.325E27;
+        double neptune = 4.5E+9;
         System.out.println(neptune);
         boolean greencard = true;
         System.out.println(greencard);
