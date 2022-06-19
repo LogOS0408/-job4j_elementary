f2521f04ad0b69f992dbbddc31da497c03265e2e
LogOS0408 <bsv-81@mail.ru>
2022-06-19 04:46:28 +0400

    3. Арифметические операции. [#185 #313629]

--- src/main/java/ru/job4j/VariablesTwo.java
+++ src/main/java/ru/job4j/VariablesTwo.java
@@ -10,9 +10,9 @@ public class VariablesTwo {
         System.out.println(height);
         short brick = 10400;
         System.out.println(brick);
-        double bacteria = 1.325E+27;
+        double bacteria = 1.325E27;
         System.out.println(bacteria);
-        double neptune = 1.325E+27;
+        double neptune = 1.325E27;
         System.out.println(neptune);
         boolean greencard = true;
         System.out.println(greencard);
