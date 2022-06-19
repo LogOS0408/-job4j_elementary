f3085172627cf3fcc5167a601068691bdd3af75a
LogOS0408 <bsv-81@mail.ru>
2022-06-19 15:54:15 +0400

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/Fit.java
@@ -0,0 +1,24 @@
+package ru.job4j;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double rls = (height - 100) * 1.15;
+        return rls;
+    }
+
+    public static double womanWeight(short height) {
+        double rls = (height - 110) * 1.15;
+        return rls;
+    }
+
+    public static void main(String[] args) {
+        short menheight = 187;
+        double man = Fit.manWeight(menheight);
+        System.out.println("Man 187 is " + man);
+        short womanheight = 175;
+        double woman = Fit.womanWeight(womanheight);
+        System.out.println("Woman 175 is " + woman);
+    }
+
+
+}
