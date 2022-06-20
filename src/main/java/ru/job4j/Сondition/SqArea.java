a0357c8b5ef8dff0e9fdcb9aa405d0c536c6ec9b
LogOS0408 <bsv-81@mail.ru>
2022-06-20 04:39:09 +0400

    8. Стороны прямоугольника. [#137735]

---
+++ src/main/java/ru/job4j/Сondition/SqArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.Сondition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double i = h * k;
+        double s = h * i;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        double rez = SqArea.square(6,2);
+        System.out.println("Square is " + rez);
+    }
+}
