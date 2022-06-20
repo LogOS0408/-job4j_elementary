780025e14ab419a627b1203eca6e528cb3542069
LogOS0408 <bsv-81@mail.ru>
2022-06-20 05:40:43 +0400

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/Сondition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.Сondition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java !";
+        System.out.println(idea);
+        idea = idea + " But I'm newbie ";
+        int ear = 2022;
+        idea += ear;
+        System.out.println(idea);
+    }
+}
