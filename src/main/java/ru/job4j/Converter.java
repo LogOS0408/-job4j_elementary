246f9b0a86bda9e994c8a771154b46a6da49b883
LogOS0408 <bsv-81@mail.ru>
2022-06-25 14:48:22 +0400

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -14,7 +14,7 @@ public class Converter {
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro");
+        System.out.println("140 rubles are " + euro + " euro ");
         float dollar = Converter.rubleToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollar");
         float in = 140;
