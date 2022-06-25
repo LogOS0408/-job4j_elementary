15953d31ddb38b986a586651d05931eab502528c
LogOS0408 <bsv-81@mail.ru>
2022-06-25 14:33:00 +0400

    9.1. String [#237407]

--- src/main/java/ru/job4j/Greeting.java
+++ src/main/java/ru/job4j/Greeting.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java !";
+        String idea = "I like Java ! ";
         System.out.println(idea);
         idea = idea + " But I'm newbie ";
         int ear = 2022;
