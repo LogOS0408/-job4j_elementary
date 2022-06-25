543fb51719199803eada709a723d08cb27d36550
LogOS0408 <bsv-81@mail.ru>
2022-06-25 14:08:56 +0400

    8. Стороны прямоугольника. [#137735 #316185]

--- src/main/java/ru/job4j/SqArea.java
+++ src/main/java/ru/job4j/SqArea.java
@@ -9,7 +9,7 @@ public class SqArea {
     }
 
     public static void main(String[] args) {
-        double rez = SqArea.square(10, 6);
+        double rez = SqArea.square(6, 2);
         System.out.println("Square is " + rez);
     }
 }
