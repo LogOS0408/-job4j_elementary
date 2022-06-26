802b6d5ee47cd3c9d01232c46fc4ba4236ea8461
LogOS0408 <bsv-81@mail.ru>
2022-06-26 11:33:49 +0400

    5. Конвертер валюты [#41583 #316179]

---
+++ src/main/java/ru/job4j/converter/ConverterRub.java
@@ -0,0 +1,22 @@
+package ru.job4j.converter;
+
+public class ConverterRub {
+    public class Converter {
+        public static float rubleToEuro(float value) {
+            float rls = value / 70;
+            return rls;
+
+        }
+
+        public static float rubleToDollar(float value) {
+            float rls = value / 60;
+            return rls;
+        }
+
+        public static void main(String[] args) {
+            float euro = ru.job4j.converter.Converter.rubleToEuro(140);
+            System.out.println("140 rubles are " + euro + " euro ");
+            float dollar = ru.job4j.converter.Converter.rubleToDollar(180);
+            System.out.println("180 rubles are " + dollar + " dollar");
+        }
+}
