521ffdba7c1e0ee4bc4816a34e8a3066870d4f4c
LogOS0408 <bsv-81@mail.ru>
2022-07-02 12:30:17 +0400

    4.1.2. Глупый бот [#31323]

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String rls = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот".equals(question)) {
+            rls = "Привет, умник";
+        } else if ("Пока".equals(question)) {
+            rls = "До скорой встречи";
+        }
+        return rls;
+    }
+
+    public static void main(String[] args) {
+        String rls = DummyBot.answer("Привет, Бот");
+        System.out.println(rls);
+        rls = DummyBot.answer("Пока");
+        System.out.println(rls);
+        rls = DummyBot.answer("Сколько будет 2+2 ?");
+        System.out.println(rls);
+
+    }
+}
