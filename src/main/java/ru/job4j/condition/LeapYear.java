8aa0af37da0d84d6cf41f9ce8cbd645cb6c68fff
LogOS0408 <bsv-81@mail.ru>
2022-07-15 10:57:25 +0400

    1.5. Високосный год.

---
+++ src/main/java/ru/job4j/condition/LeapYear.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class LeapYear {
+    @SuppressWarnings("checkstyle:SimplifyBooleanReturn")
+    public static boolean checkYear(int year) {
+        if (year % 400 == 0 && year % 100 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        }
+        return false;
+    }
+}
