1051c47031f81438fa76c069cd12faf64dd0a675
LogOS0408 <bsv-81@mail.ru>
2022-07-15 12:10:13 +0400

    1.7. Ладьи на шахматной доске

---
+++ src/main/java/ru/job4j/condition/AttackRook.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class AttackRook {
+    public static void main(String[] args) {
+               System.out.println(check("A8", "A9"));
+    }
+
+    public static boolean check(String left, String right) {
+        char a = left.charAt(0);
+        char b = left.charAt(1);
+        char c = right.charAt(0);
+        char d = right.charAt(1);
+        if (a == c) {
+            return true;
+        } else if (b == d) {
+            return true;
+        }
+        return false;
+    }
+}
