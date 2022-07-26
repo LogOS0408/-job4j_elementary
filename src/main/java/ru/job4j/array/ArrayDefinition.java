f8eeccf7a2616cc23a965ee00f17dfe2bdc83a3f
LogOS0408 <bsv-81@mail.ru>
2022-07-26 20:13:19 +0400

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен ages: " + ages.length);
         System.out.println("Размер массива равен surnames: " + surnames.length);
         System.out.println("Размер массива равен prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Stanislav Batuev";
+        names[1] = "Ivan Batuev";
+        names[2] = "Nataliya Batueva";
+        names[3] = "Dasha Yamshikova";
+        System.out.println("Имя под индексом 0 - " + names[0]);
+        System.out.println("Имя под индексом 1 - " + names[1]);
+        System.out.println("Имя под индексом 2 - " + names[2]);
+        System.out.println("Имя под индексом 3 - " + names[3]);
+
     }
 }
