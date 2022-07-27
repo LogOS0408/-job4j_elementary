6a6a37e2cee1663742ab9792b2bf02fee9f17f33
LogOS0408 <bsv-81@mail.ru>
2022-07-27 11:07:15 +0400

    6.0.4. Массивы и цикл for. [#226665]

--- src/main/java/ru/job4j/array/Loop.java
+++ src/main/java/ru/job4j/array/Loop.java
@@ -1,4 +1,13 @@
 package ru.job4j.array;
 
 public class Loop {
+    public static void main(String[] args) {
+        int[] task = new int[5];
+        for (int i = 0; i < task.length; i++) {
+            task[i] = i * 2 + 3;
+        }
+        for (int i = 0; i < task.length; i++) {
+            System.out.println(task[i]);
+        }
+    }
 }
