eb73566fa35ca9a49e899b7f70714d6cba0b5a94
LogOS0408 <bsv-81@mail.ru>
2022-07-28 13:20:28 +0400

    6.1. Классический поиск перебором. [#33489]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int[] num = new int[]{1, 10, 3};
+        System.out.println(indexOf(num, 2));
+    }
+}
