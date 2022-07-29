8c85bac1f126df140be82abd3521703e819e645f
LogOS0408 <bsv-81@mail.ru>
2022-07-29 10:51:25 +0400

    0.2. Вывести на консоль числа от N до M

---
+++ src/main/java/ru/job4j/loop/Print0ToN.java
@@ -0,0 +1,10 @@
+package ru.job4j.loop;
+
+public class Print0ToN {
+
+    public static void out(int n, int m) {
+        for (int i = n; i < m; i++) {
+            System.out.println(i);
+        }
+    }
+}
