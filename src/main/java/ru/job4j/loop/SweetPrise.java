abe016099de27c817700d8a3317228cf2337e16d
LogOS0408 <bsv-81@mail.ru>
2022-07-30 21:36:48 +0400

    0.5. Вывести стоимость конфет за килограмм

---
+++ src/main/java/ru/job4j/loop/SweetPrise.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class SweetPrise {
+    public static void out(int price) {
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum = price * i;
+            System.out.println(sum);
+        }
+    }
+}
