d64aea2d923aa603dbdf0d803d1b03e57b027f84
LogOS0408 <bsv-81@mail.ru>
2022-07-31 13:23:07 +0400

    0.9. Найти количество нечетных в диапазоне

--- src/main/java/ru/job4j/loop/OddCount.java
+++ src/main/java/ru/job4j/loop/OddCount.java
@@ -2,12 +2,12 @@ package ru.job4j.loop;
 
 public class OddCount {
     public static int count(int a, int b) {
-        int count = 0;
+        int counts = 0;
         for (int i = a; i <= b; i++) {
             if (i % 2 != 0) {
-                count++;
+                counts++;
             }
         }
-        return count;
+        return counts;
     }
 }
