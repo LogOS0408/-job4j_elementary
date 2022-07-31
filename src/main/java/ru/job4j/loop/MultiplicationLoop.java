dde0769ae234b41b3266c82f347422cf27e65e49
LogOS0408 <bsv-81@mail.ru>
2022-07-31 12:44:08 +0400

    0.6. Произведение чисел в диапазоне

---
+++ src/main/java/ru/job4j/loop/MultiplicationLoop.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class MultiplicationLoop {
+    public static int mult(int a, int b) {
+        int mtp = 1;
+        for (int i = a; i <= b; i++) {
+            mtp = mtp * i;
+            System.out.println(mtp);
+        }
+        return mtp;
+    }
+}
