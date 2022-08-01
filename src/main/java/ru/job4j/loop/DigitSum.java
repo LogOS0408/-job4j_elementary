d9f5d4508621f127548356e7931b6e8acfac0dea
LogOS0408 <bsv-81@mail.ru>
2022-08-01 17:33:23 +0400

    1.4. Сумма цифр

---
+++ src/main/java/ru/job4j/loop/DigitSum.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class DigitSum {
+    public static int sum(int num) {
+        int sum = 0;
+        while (num != 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(34));
+
+    }
+}
