13d561d119dc78c16b56234c16458742d091a333
LogOS0408 <bsv-81@mail.ru>
2022-08-01 12:16:18 +0400

    1.2. Найти количество отрезков, используя цикл

---
+++ src/main/java/ru/job4j/loop/SectionCount.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class SectionCount {
+    public static int count(int length, int section) {
+        int sum = 0;
+        while (section <= length) {
+            section = section + section;
+            sum++;
+        }
+        return sum;
+    }
+}
