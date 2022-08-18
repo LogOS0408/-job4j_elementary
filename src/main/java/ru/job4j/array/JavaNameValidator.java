fad44280c5cbd3d975f1a15ed27849ee353832cc
LogOS0408 <bsv-81@mail.ru>
2022-08-18 10:54:57 +0400

    6.9. String - это массив [#504884 #333598]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,19 +2,36 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean result = false;
+        boolean result = !name.isEmpty();
         for (int i = 0; i < name.length(); i++) {
             int code = name.codePointAt(i);
-            int firstsymbol = name.codePointAt(0);
-            if ((firstsymbol >= 48 && firstsymbol <= 57) || (firstsymbol >= 65 && firstsymbol <= 90)) {
-                result = false;
-                break;
-            } else if ((firstsymbol >= 97 && firstsymbol <= 122) || (code >= 65 && code <= 90) || code == 36 || code == 95 || (code >= 48 && code <= 57)) {
-                result = true;
-            } else {
-                return result;
+            if (isUpperLatinLetter(name.codePointAt(0))
+                    || isSpecialSymbol(name.codePointAt(0))
+                    || isFrom0To10(name.codePointAt(0))) {
+                return false;
+            } else if (isLowerLatinLetter(code)
+                    || isUpperLatinLetter(code)
+                    || isSpecialSymbol(code)
+                    || isFrom0To10(code)) {
+                return true;
             }
         }
         return result;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+
+    public static boolean isFrom0To10(int code) {
+        return code >= 48 && code <= 57;
     }
 }
