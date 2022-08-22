77d98526baf2b69212cf9aa56cad9bd9749f57b0
LogOS0408 <bsv-81@mail.ru>
2022-08-22 07:28:42 +0400

    Tasks

---
+++ src/main/java/ru/job4j/tasks/ForAmp.java
@@ -0,0 +1,17 @@
+package ru.job4j.tasks;
+
+public class ForAmp {
+    public static boolean experimentone(char[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 'A' || array[i] == 'B' && array[i] == 'C') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        char[] array = {'A', 'B', 'C'};
+        System.out.println(experimentone(array));
+    }
+}
