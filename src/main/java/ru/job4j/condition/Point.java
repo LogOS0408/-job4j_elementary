c4425154b6832a8f44562cf1d250b73ecd037bd4
LogOS0408 <bsv-81@mail.ru>
2022-09-07 16:56:19 +0400

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,26 @@
 package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double a = (x2 - x1);
-        double b = (y2 - y1);
-        double c = Math.pow(a, 2);
-        double d = Math.pow(b, 2);
-        double i = c + d;
-        double f = Math.sqrt(i);
-        return f;
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double rez = Point.distance(6, 94, 15, 150);
-        System.out.println(rez);
+        Point a = new Point(13, 0);
+        Point b = new Point(0, 13);
+        double dist = a.distance(b);
+        System.out.println(dist);
+
     }
 }
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    public void when0002Then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        a.distance(b);
+
+        double expected = 2.0;
+        assertThat(a.distance(b)).isEqualTo(expected);
+    }
+
+    @Test
+    public void when0400Then4() {
+        Point a = new Point(0, 4);
+        Point b = new Point(0, 0);
+        a.distance(b);
+        double expected = 4.0;
+        assertThat(a.distance(b)).isEqualTo(expected);
+    }
+
+    @Test
+    public void when130013Then18() {
+        Point a = new Point(13, 0);
+        Point b = new Point(0, 13);
+        a.distance(b);
+        double expected = 18.384776310850235;
+        assertThat(a.distance(b)).isEqualTo(expected);
+    }
+}
